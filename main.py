c96358e8315bea7def78ed16f6edc2db4d05469f
Anton <antzotik@gmail.com>
2023-04-11 12:32:38 +0300

    init project

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Body
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi import responses
+import polyline
+import json
+import requests
+
+app = FastAPI()
+ 
+app.mount("/front", StaticFiles(directory="front"),name="front")
+
+@app.get("/")
+def root():
+    return FileResponse("front/map.html")
+
+ 
+@app.post("/hello")
+#def hello(name = Body(embed=True)):
+def hello(data = Body()):
+    name = data["lat"]
+    age = data["lng"]
+    return {"message": f"lng: {name}, lat: {age}"}
+
+
+
+
+"""
+    Получение строки координат и передача их на API OSRM
+    res Получение ответа по апи и трансформация их в массив точек, которые будут отрисовываться 
+
+    overview = full|false|
+
+"""
+@app.post("/decode")
+def decode(data= Body()):
+    res = requests.get("http://router.project-osrm.org/route/v1/driving/"+data["params"]+"?geometries=polyline&overview=full")
+    js = json.loads(res.text)
+    return polyline.decode(js['routes'][0]["geometry"])
