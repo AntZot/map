96cf8a4bee4b213f3a7dc54cc8c0982e14085494
Anton Zotov <antzotik@gmail.com>
2024-06-04 18:52:41 +0300

    vercel test

--- src/back/main.py
+++ src/back/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Body, Response, status
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi import responses 
-import polyline
-import json
-import requests
-from recomendation_gen import recomendation 
-from operator import itemgetter
-import numpy as np
+# from fastapi import responses 
+# import polyline
+# import json
+# import requests
+# from recomendation_gen import recomendation 
+# from operator import itemgetter
+# import numpy as np
 import uvicorn
 
 
@@ -20,22 +20,14 @@ app.mount("/front", StaticFiles(directory="front"), name="front")
 
 startup_data = {}
 
-@app.on_event("startup")
-async def startup_event():
-    startup_data["rec"] = recomendation()
+# @app.on_event("startup")
+# async def startup_event():
+#     startup_data["rec"] = recomendation()
 
 
 @app.get("/")
 def root():
     return FileResponse("front/map.html")
-
- 
-@app.post("/hello")
-def get_coord(data = Body()):
-    name = data["lat"]
-    age = data["lng"]
-    return {"message": f"lng: {name}, lat: {age}"}
-
 
 """
     Получение строки координат и передача их на API OSRM
@@ -44,43 +36,43 @@ def get_coord(data = Body()):
     overview = full|false|
 
 """
-@app.post("/decode",status_code=201)
-def decode(response: Response, data= Body()):
-    params = ''
-    points = data['params']
-    points_var = np.var(np.array(points),axis=0,dtype=np.float64)
-    print("     "+ f"{points_var}")
-    if points_var[0] > points_var[1]:
-        points = sorted(points,reverse=True) 
-    else:
-        points = sorted(points, key=itemgetter(1))
+# @app.post("/decode",status_code=201)
+# def decode(response: Response, data= Body()):
+#     params = ''
+#     points = data['params']
+#     points_var = np.var(np.array(points),axis=0,dtype=np.float64)
+#     print("     "+ f"{points_var}")
+#     if points_var[0] > points_var[1]:
+#         points = sorted(points,reverse=True) 
+#     else:
+#         points = sorted(points, key=itemgetter(1))
 
-    #Сборка строки для отправки запросса API
-    for count, point in enumerate(points):
-        params += f"{point[1]},{point[0]}"
-        if(count != (len(points)-1)):
-            params += ";"
-    print("     "+params)
+#     #Сборка строки для отправки запросса API
+#     for count, point in enumerate(points):
+#         params += f"{point[1]},{point[0]}"
+#         if(count != (len(points)-1)):
+#             params += ";"
+#     print("     "+params)
 
-    res = requests.get(OSRM_API_URL+params+"?geometries=polyline&overview=full") 
-    js = json.loads(res.text)
-    if js["code"]=='NoRoute': 
-        resp = {"message":"Impossible route between points"}
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    else:
-        resp = polyline.decode(js['routes'][0]["geometry"])
-    return resp
+#     res = requests.get(OSRM_API_URL+params+"?geometries=polyline&overview=full") 
+#     js = json.loads(res.text)
+#     if js["code"]=='NoRoute': 
+#         resp = {"message":"Impossible route between points"}
+#         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+#     else:
+#         resp = polyline.decode(js['routes'][0]["geometry"])
+#     return resp
 
 
-"""
-    Создание рекомендаций и отправка их на фронтенд
-    data - список пар координат на основе которых создаются рекомендации 
+# """
+#     Создание рекомендаций и отправка их на фронтенд
+#     data - список пар координат на основе которых создаются рекомендации 
     
-"""
-@app.post("/recomend")
-def recomend(data= Body()):
-    content = startup_data["rec"].get_recomendation(data["data"],data["params"])
-    return responses.JSONResponse(content=content,media_type="application/json")
+# """
+# @app.post("/recomend")
+# def recomend(data= Body()):
+#     content = startup_data["rec"].get_recomendation(data["data"],data["params"])
+#     return responses.JSONResponse(content=content,media_type="application/json")
 
 
 if __name__ == "__main__":
